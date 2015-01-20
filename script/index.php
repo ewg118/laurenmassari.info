143141204f1016f23b2e6d88d1fe930970e1af1d
Ethan Gruber <ewg4xuva@gmail.com>
2015-01-20 08:59:34 -0500

    added meta tags for bootstrap

--- script/index.php
+++ script/index.php
@@ -38,6 +38,9 @@
 		<!-- bootstrap -->
 		<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.1/css/bootstrap.min.css"/>
 		<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.1/js/bootstrap.min.js">//</script>
+                <meta charset="utf-8">
+                <meta http-equiv="X-UA-Compatible" content="IE=edge">
+                <meta name="viewport" content="width=device-width, initial-scale=1">
 
 		<!-- local styling -->
 		<link href="<?php echo $display_path . 'css/style.css' ?>" rel="stylesheet"/>
